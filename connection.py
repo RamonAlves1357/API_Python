022bbe50733cd9ddb6787e86410c650d2110f0bb
igorsilva3 <igorbr2018@gmail.com>
2020-10-02 20:55:13 -0300

    Refatoracao

--- connection.py
+++ connection.py
@@ -1,7 +1,14 @@
-import mysql.connector
+from mysql import connector
 
 def criar_conexao(host, user, senha, banco):
-    return mysql.connector.connect(host=host, user=user, password=senha, database=banco)
+    mydb = connector.connect(
+        host=host, 
+        user=user, 
+        password=senha, 
+        database=banco
+    )
+    
+    return mydb
 
 def fechar_conexao(con):
     return con.close()
